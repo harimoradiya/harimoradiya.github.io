7f1ccadb4cf1f06df12d629fc6de321c80917139
harimoradiya <harimoradiya123@gmail.com>
2024-05-28 13:43:51 +0530

    Updates

---
+++ _app/immutable/chunks/unitto-logo.bd9169ca.js
@@ -0,0 +1,1 @@
+const o=""+new URL("../assets/unitto-logo.12c4dca9.svg",import.meta.url).href;export{o as u};
---
+++ _app/immutable/nodes/7.64283a62.js
@@ -0,0 +1,56 @@
+import{s as ue,a as r,f as p,c as l,g as u,D as h,i as n,d as a,l as K,m as N}from"../chunks/scheduler.c8f7af58.js";import{S as he,i as de,b as z,d as B,m as D,a as G,t as O,e as J}from"../chunks/index.88d880f6.js";import{W as fe,T as me,a as pe}from"../chunks/WallOfText.168da1a7.js";function ce(d){let o;return{c(){o=K("Terms & Conditions")},l(i){o=N(i,"Terms & Conditions")},m(i,s){n(i,o,s)},d(i){i&&a(o)}}}function ye(d){let o;return{c(){o=K("Changes to This Terms and Conditions")},l(i){o=N(i,"Changes to This Terms and Conditions")},m(i,s){n(i,o,s)},d(i){i&&a(o)}}}function ve(d){let o;return{c(){o=K("Contact Us")},l(i){o=N(i,"Contact Us")},m(i,s){n(i,o,s)},d(i){i&&a(o)}}}function we(d){let o,i,s,c=`By downloading or using the app, these terms will automatically apply to you
+    – you should make sure therefore that you read them carefully before using
+    the app. You’re not allowed to copy or modify the app, any part of the app,
+    or our trademarks in any way. You’re not allowed to attempt to extract the
+    source code of the app, and you also shouldn’t try to translate the app into
+    other languages or make derivative versions. The app itself, and all the
+    trademarks, copyright, database rights, and other intellectual property
+    rights related to it, still belong to Sad Ellie.`,y,v,Q=`Sad Ellie is committed to ensuring that the app is as useful and efficient
+    as possible. For that reason, we reserve the right to make changes to the
+    app or to charge for its services, at any time and for any reason. We will
+    never charge you for the app or its services without making it very clear to
+    you exactly what you’re paying for.`,S,w,R=`The Unitto app stores and processes personal data that you have provided to
+    us, to provide my Service. It’s your responsibility to keep your phone and
+    access to the app secure. We therefore recommend that you do not jailbreak
+    or root your phone, which is the process of removing software restrictions
+    and limitations imposed by the official operating system of your device. It
+    could make your phone vulnerable to malware/viruses/malicious programs,
+    compromise your phone’s security features and it could mean that the Unitto
+    app won’t work properly or at all.`,E,x,V=`You should be aware that there are certain things that Sad Ellie will not
+    take responsibility for. Certain functions of the app will require the app
+    to have an active internet connection. The connection can be Wi-Fi or
+    provided by your mobile network provider, but Sad Ellie cannot take
+    responsibility for the app not working at full functionality if you don’t
+    have access to Wi-Fi, and you don’t have any of your data allowance left.`,W,$,X=`If you’re using the app outside of an area with Wi-Fi, you should remember
+    that the terms of the agreement with your mobile network provider will still
+    apply. As a result, you may be charged by your mobile provider for the cost
+    of data for the duration of the connection while accessing the app, or other
+    third-party charges. In using the app, you’re accepting responsibility for
+    any such charges, including roaming data charges if you use the app outside
+    of your home territory (i.e. region or country) without turning off data
+    roaming. If you are not the bill payer for the device on which you’re using
+    the app, please be aware that we assume that you have received permission
+    from the bill payer for using the app.`,I,g,Z=`Along the same lines, Sad Ellie cannot always take responsibility for the
+    way you use the app i.e. You need to make sure that your device stays
+    charged – if it runs out of battery and you can’t turn it on to avail the
+    Service, Sad Ellie cannot accept responsibility.`,q,b,ee=`With respect to Sad Ellie’s responsibility for your use of the app, when
+    you’re using the app, it’s important to bear in mind that although we
+    endeavor to ensure that it is updated and correct at all times, we do rely
+    on third parties to provide information to us so that we can make it
+    available to you. Sad Ellie accepts no liability for any loss, direct or
+    indirect, you experience as a result of relying wholly on this functionality
+    of the app.`,A,C,te=`At some point, we may wish to update the app. The app is currently available
+    on Android – the requirements for the system(and for any additional systems
+    we decide to extend the availability of the app to) may change, and you’ll
+    need to download the updates if you want to keep using the app. Sad Ellie
+    does not promise that it will always update the app so that it is relevant
+    to you and/or works with the Android version that you have installed on your
+    device. However, you promise to always accept updates to the application
+    when offered to you, We may also wish to stop providing the app, and may
+    terminate use of it at any time without giving notice of termination to you.
+    Unless we tell you otherwise, upon any termination, (a) the rights and
+    licenses granted to you in these terms will end; (b) you must stop using the
+    app, and (if needed) delete it from your device.`,H,f,U,_,oe=`I may update our Terms and Conditions from time to time. Thus, you are
+    advised to review this page periodically for any changes. I will notify you
+    of any changes by posting the new Terms and Conditions on this page.`,F,T,ne="These terms and conditions are effective as of 2023-01-18",L,m,M,k,ae=`If you have any questions or suggestions about my Terms and Conditions, do
+    not hesitate to contact me at <a class="font-medium text-blue-700 visited:text-fuchsia-700 hover:text-blue-900 hover:underline" href="mailto:sadellie.dev@gmail.com">sadellie.dev@gmail.com</a>`,Y,P,ie='This Terms and Conditions page was generated by <a class="font-medium text-blue-700 visited:text-fuchsia-700 hover:text-blue-900 hover:underline" href="https://app-privacy-policy-generator.nisrulz.com/">App Privacy Policy Generator</a>',j;return o=new me({props:{$$slots:{default:[ce]},$$scope:{ctx:d}}}),f=new pe({props:{$$slots:{default:[ye]},$$scope:{ctx:d}}}),m=new pe({props:{$$slots:{default:[ve]},$$scope:{ctx:d}}}),{c(){z(o.$$.fragment),i=r(),s=p("p"),s.textContent=c,y=r(),v=p("p"),v.textContent=Q,S=r(),w=p("p"),w.textContent=R,E=r(),x=p("p"),x.textContent=V,W=r(),$=p("p"),$.textContent=X,I=r(),g=p("p"),g.textContent=Z,q=r(),b=p("p"),b.textContent=ee,A=r(),C=p("p"),C.textContent=te,H=r(),z(f.$$.fragment),U=r(),_=p("p"),_.textContent=oe,F=r(),T=p("p"),T.textContent=ne,L=r(),z(m.$$.fragment),M=r(),k=p("p"),k.innerHTML=ae,Y=r(),P=p("p"),P.innerHTML=ie},l(e){B(o.$$.fragment,e),i=l(e),s=u(e,"P",{"data-svelte-h":!0}),h(s)!=="svelte-j6upou"&&(s.textContent=c),y=l(e),v=u(e,"P",{"data-svelte-h":!0}),h(v)!=="svelte-bwk9q0"&&(v.textContent=Q),S=l(e),w=u(e,"P",{"data-svelte-h":!0}),h(w)!=="svelte-16m1frx"&&(w.textContent=R),E=l(e),x=u(e,"P",{"data-svelte-h":!0}),h(x)!=="svelte-1rcxa67"&&(x.textContent=V),W=l(e),$=u(e,"P",{"data-svelte-h":!0}),h($)!=="svelte-cyvruw"&&($.textContent=X),I=l(e),g=u(e,"P",{"data-svelte-h":!0}),h(g)!=="svelte-hg0x5m"&&(g.textContent=Z),q=l(e),b=u(e,"P",{"data-svelte-h":!0}),h(b)!=="svelte-1ikfww7"&&(b.textContent=ee),A=l(e),C=u(e,"P",{"data-svelte-h":!0}),h(C)!=="svelte-1b666r7"&&(C.textContent=te),H=l(e),B(f.$$.fragment,e),U=l(e),_=u(e,"P",{"data-svelte-h":!0}),h(_)!=="svelte-1epwa8h"&&(_.textContent=oe),F=l(e),T=u(e,"P",{"data-svelte-h":!0}),h(T)!=="svelte-1qbpabm"&&(T.textContent=ne),L=l(e),B(m.$$.fragment,e),M=l(e),k=u(e,"P",{"data-svelte-h":!0}),h(k)!=="svelte-1nvnlep"&&(k.innerHTML=ae),Y=l(e),P=u(e,"P",{"data-svelte-h":!0}),h(P)!=="svelte-yxljeh"&&(P.innerHTML=ie)},m(e,t){D(o,e,t),n(e,i,t),n(e,s,t),n(e,y,t),n(e,v,t),n(e,S,t),n(e,w,t),n(e,E,t),n(e,x,t),n(e,W,t),n(e,$,t),n(e,I,t),n(e,g,t),n(e,q,t),n(e,b,t),n(e,A,t),n(e,C,t),n(e,H,t),D(f,e,t),n(e,U,t),n(e,_,t),n(e,F,t),n(e,T,t),n(e,L,t),D(m,e,t),n(e,M,t),n(e,k,t),n(e,Y,t),n(e,P,t),j=!0},p(e,t){const se={};t&1&&(se.$$scope={dirty:t,ctx:e}),o.$set(se);const re={};t&1&&(re.$$scope={dirty:t,ctx:e}),f.$set(re);const le={};t&1&&(le.$$scope={dirty:t,ctx:e}),m.$set(le)},i(e){j||(G(o.$$.fragment,e),G(f.$$.fragment,e),G(m.$$.fragment,e),j=!0)},o(e){O(o.$$.fragment,e),O(f.$$.fragment,e),O(m.$$.fragment,e),j=!1},d(e){e&&(a(i),a(s),a(y),a(v),a(S),a(w),a(E),a(x),a(W),a($),a(I),a(g),a(q),a(b),a(A),a(C),a(H),a(U),a(_),a(F),a(T),a(L),a(M),a(k),a(Y),a(P)),J(o,e),J(f,e),J(m,e)}}}function xe(d){let o,i;return o=new fe({props:{$$slots:{default:[we]},$$scope:{ctx:d}}}),{c(){z(o.$$.fragment)},l(s){B(o.$$.fragment,s)},m(s,c){D(o,s,c),i=!0},p(s,[c]){const y={};c&1&&(y.$$scope={dirty:c,ctx:s}),o.$set(y)},i(s){i||(G(o.$$.fragment,s),i=!0)},o(s){O(o.$$.fragment,s),i=!1},d(s){J(o,s)}}}class Ce extends he{constructor(o){super(),de(this,o,null,xe,ue,{})}}export{Ce as component};
