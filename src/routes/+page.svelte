c2b9fc5ce47ec354935fbe5bab6682215028a34c
Hari Moradiya <66943772+harimoradiya@users.noreply.github.com>
2024-06-11 18:25:12 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,11 +17,6 @@
 	import Github from "./Github.svelte";
 	import Telegram from "./Telegram.svelte";
 </script>
-<svelte:head>
-	<link rel="stylesheet" href="/stylesheets/{selected}.css" />
-    <title>Hari Moradiya</title>
-</svelte:head>
-
 <div class="flex flex-col place-items-center p-8 bg-blue-50 dark:bg-blue-950 text-black dark:text-white">
 	<div
 		class="flex flex-col py-16 xl:py-32 rounded-3xl place-items-center gap-8 bg-center bg-cover w-full relative dark:invert"
