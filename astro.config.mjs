532b6ea33d6bbdb7c4d7e88932d10cffdfa23cf6
harimoradiya <harimoradiya123@gmail.com>
2025-06-05 10:00:29 +0530

    Changed astro-config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     site: 'https://harimoradiya.github.io',
-    base: '/harimoradiya.github.io',
+    base: 'harimoradiya.github.io',
   })
