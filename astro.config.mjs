4374ff84f527b2c21287daba336ab881044cfe68
Harikrushn Moradiya <66943772+harimoradiya@users.noreply.github.com>
2025-06-07 22:33:35 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 // export default {}
+
+
 export default defineConfig({
     site: 'https://harimoradiya.github.io',
     base: 'harimoradiya.github.io',
